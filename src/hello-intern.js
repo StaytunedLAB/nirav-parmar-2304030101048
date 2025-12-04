5fc8e05c3137b89d7b72441b63f7154e4e2f2832
parmar nirav <2304030101048@silveroakuni.ac.in>
2025-12-04 11:45:46 +0530

    Add game HTML and JavaScript for "Catch the Falling Objects" game

--- src/hello-intern.js
+++ src/hello-intern.js
@@ -1,7 +1,8 @@
-// src/hello-intern.js
-// This is a simple script to confirm your Node.js environment is set up correctly.
-
-console.log("-----------------------------------------");
-console.log("🚀 Hello Intern! Welcome to StaytunedLABS!");
-console.log("-----------------------------------------");
-console.log("Your environment is ready. You can now start coding!");
+<!DOCTYPE html>
+<html>
+  <body>
+    <script>
+      console.log("Hello, World!");
+    </script>
+  </body>
+</html>
